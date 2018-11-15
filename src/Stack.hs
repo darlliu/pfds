9332b9eccfe1cd560064399bf6e3ea1f0ad7ad17
darlliu <darlliu@lavabit.com>
2018-11-14 23:13:13 -0800

    implemented basic list

--- src/Stack.hs
+++ src/Stack.hs
@@ -1,5 +1,31 @@
 module Stack
 where
+import Prelude hiding (empty, isEmpty, cons, head, tail)
 
-someFunc :: String
-someFunc = "Test"
+someFunc = "Hello, World"
+
+class Stack s where
+    empty :: s a
+    isEmpty :: s a -> Bool
+    cons :: a -> s a -> s a
+    head :: s a -> a
+    tail :: s a -> s a 
+
+data List a = Empty | MyCons a (List a)
+    deriving (Show, Read, Ord, Eq)
+
+
+instance Stack List where
+    empty = Empty
+    isEmpty s = case s of 
+        Empty -> True
+        _ -> False
+    cons a s = case s of
+        Empty -> MyCons a Empty
+        s' -> MyCons a s'
+    head (MyCons a _)  = a
+    tail Empty = empty
+    tail (MyCons _ s) = s
+
+singleton :: a -> List a
+singleton a = cons a Empty
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,30 @@
 import Test.HUnit
 import PurelyFunctional
+import Prelude hiding (empty, isEmpty, cons, head, tail)
+
+myList1  = singleton 1
+myList2  = 4 `cons` (3 `cons` (2 `cons` myList1))
+myList3  = 3 `cons` (2 `cons` myList1)
 
 testHello =
     TestCase $ assertEqual "Testing basic import"
     (someFunc) (someFunc)
 
+testHead =
+    TestCase $ assertEqual "Test List: Head"
+    4 (head myList2)
+
+testTail =
+    TestCase $ assertEqual "Test List: Tail"
+    (myList3) (tail myList2)
+
+testIsEmpty = 
+    TestCase $ assertEqual "Test List: IsEmpty"
+    (False) (isEmpty myList1)
+
+testIsEmpty2 = 
+    TestCase $ assertEqual "Test List: IsEmpty 2"
+    (True) (isEmpty Empty)
+
 main :: IO Counts
-main = runTestTT $ TestList [ testHello ]
+main = runTestTT $ TestList [ testHello, testHead, testTail, testIsEmpty, testIsEmpty2 ]
